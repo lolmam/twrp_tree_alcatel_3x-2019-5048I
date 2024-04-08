c32778ccbd7a393af4feb8203f8ac23ce9b6bddf
lol.mam <69152475+lolmam@users.noreply.github.com>
2024-04-09 01:04:09 +0300

    Removed Tutorial Stuff

--- recovery/root/sbin/permissive.sh
+++ recovery/root/sbin/permissive.sh
@@ -2,7 +2,6 @@
 
 setenforce 0
 
-# Get your device's block path where "system", "recovery", etc. lives.
-# That can be "/dev/block/bootdevice/by-name" or something like that.
+# Device's block path where "system", "recovery", etc. lives.
 mkdir -p /dev/block/by-name/
 busybox mount -o bind /dev/block/by-name/ /dev/block/platform/bootdevice/by-name
